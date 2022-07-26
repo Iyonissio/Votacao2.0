dbd2e709d30db91a45682798b5c7dff9d244a7ca
Iyonissio <mbryoyo01@gmail.com>
2022-07-26 16:10:11 +0200

    update

--- polls/admin.py
+++ polls/admin.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice, Eleitor
 
-admin.site.site_header = "Pollster Admin"
-admin.site.site_title = "Pollster Admin Area"
-admin.site.index_title = "Welcome to the Pollster admin area"
+admin.site.site_header = "Administração Mesa de Voto"
+admin.site.site_title = "Ola"
+admin.site.index_title = "Bem vindo(a)"
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+#     extra = 3
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields': ['question_text']}),
-                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
-    inlines = [ChoiceInline]
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [(None, {'fields': ['question_text']}),
+#                  ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+#     inlines = [ChoiceInline]
 
 
 # admin.site.register(Question)
 # admin.site.register(Choice)
-admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question, QuestionAdmin)
 admin.site.register(Eleitor)
